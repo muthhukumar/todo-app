63f467af199c43f725218a80392114f2b243923b
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-22 18:49:47 +0530

    formatted footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
 import { Flex, FlexProps } from '@chakra-ui/react'
 
-export const Footer = (props: FlexProps) => (
-  <Flex as="footer" py="2rem" {...props} />
-)
+export const Footer = (props: FlexProps) => <Flex as="footer" py="2rem" {...props} />
