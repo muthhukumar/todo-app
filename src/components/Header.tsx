68d3a84351f6ad25830275f09cf6e348431f659f
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-23 21:40:40 +0530

    cleanups and made the header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,25 +22,22 @@ import {
   BreadcrumbItem,
   useColorModeValue,
   chakra,
-  HTMLChakraProps,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/client'
 import { signIn, signOut } from 'next-auth/client'
+import { useViewportScroll } from 'framer-motion'
 
 import { DarkModeSwitch, ThemeSwitchButton } from './DarkModeSwitch'
 import { pathProps } from '../utils/navigation'
 import IconDark from '../public/svg/icon-dark.svg'
 import IconLight from '../public/svg/icon-light.svg'
-import { useViewportScroll } from 'framer-motion'
 
 export const Header = () => {
-  const { colorMode } = useColorMode()
   const [session, loading] = useSession()
 
-  const bgColor = { light: 'white', dark: 'black' }
-
-  const color = { light: 'black', dark: 'white' }
+  const bgColor = useColorModeValue('white', 'black')
+  const color = useColorModeValue('black', 'white')
 
   const loggedIn = Boolean(session)
 
@@ -49,7 +46,7 @@ export const Header = () => {
   }
 
   return (
-    <Box bg={bgColor[colorMode]} color={color[colorMode]} borderBottomWidth="1px">
+    <Box bg={bgColor} color={color} borderBottomWidth="1px">
       {loggedIn ? <LoggedInNavigation /> : <SignInNavigation />}
     </Box>
   )
@@ -70,7 +67,6 @@ const SignInNavigation = () => {
             </Button>
             <Button onClick={() => signIn()}>Sign Up</Button>
           </ButtonGroup>
-          {/* <DarkModeSwitch /> */}
         </Flex>
       </Flex>
     </Container>
@@ -89,11 +85,7 @@ const LoggedInNavigation = () => {
 
   const Icon = icons[colorMode]
 
-  const pathName = router.pathname
-
   const projectName = router?.query?.projectName ?? ''
-
-  const getPathProps = pathProps.bind(null, pathName, colorMode)
 
   const userIdentification =
     session?.user?.name !== 'null' ? session?.user?.name : session?.user?.email
@@ -101,7 +93,7 @@ const LoggedInNavigation = () => {
   return (
     <>
       <Container maxW="container.lg" px="10">
-        <Flex flexDir="row" justifyContent="space-between" py="2" alignItems="center" pt="4">
+        <Flex flexDir="row" justifyContent="space-between" alignItems="center" pt="4">
           <Breadcrumb spacing="6">
             <BreadcrumbItem>
               <Link href="/">
@@ -129,69 +121,7 @@ const LoggedInNavigation = () => {
           <UserPopOver />
         </Flex>
       </Container>
-      <Box h="50px">
-        <StickyHeader transition="box-shadow .2s ease">
-          <Flex
-            flexDir="row"
-            alignItems="center"
-            justifyContent="flex-start"
-            maxW="container.lg"
-            px="10"
-            mx="auto"
-          >
-            <Stack direction="row" spacing="8">
-              <Link href="/">
-                <a>
-                  <Text
-                    borderBottomWidth="2px"
-                    py="3"
-                    borderBottomColor="white"
-                    {...getPathProps('/')}
-                  >
-                    Overview
-                  </Text>
-                </a>
-              </Link>
-              <Link href="/projects">
-                <a>
-                  <Text
-                    borderBottomWidth="2px"
-                    py="3"
-                    borderBottomColor="white"
-                    {...getPathProps('/projects')}
-                  >
-                    projects
-                  </Text>
-                </a>
-              </Link>
-              <Link href="/activity">
-                <a>
-                  <Text
-                    borderBottomWidth="2px"
-                    py="3"
-                    borderBottomColor="white"
-                    {...getPathProps('/activity')}
-                  >
-                    Activity
-                  </Text>
-                </a>
-              </Link>
-              <Link href="/settings">
-                <a>
-                  <Text
-                    borderBottomWidth="2px"
-                    py="3"
-                    borderBottomColor="white"
-                    {...getPathProps('/settings')}
-                  >
-                    Settings
-                  </Text>
-                </a>
-              </Link>
-            </Stack>
-          </Flex>
-        </StickyHeader>
-      </Box>
+      <StickyHeader />
     </>
   )
 }
@@ -231,40 +161,103 @@ const UserPopOver = () => {
   )
 }
 
-function StickyHeader(props: HTMLChakraProps<'header'>) {
+function StickyHeader() {
   const bg = useColorModeValue('white', 'black')
   const shadow = useColorModeValue('#0000001a', '#333')
+  const { colorMode } = useColorMode()
 
   const ref = React.useRef<HTMLHeadingElement>(null)
   const [y, setY] = React.useState(0)
-  const { height = 0 } = ref.current?.getBoundingClientRect() ?? {}
+  const { height = 50 } = ref.current?.getBoundingClientRect() ?? {}
 
-  /**
-   * NOTE:
-   * Here we are always may or will have the latest values. But why do we need to store the value in the
-   * state. So remove the state and use it as it lets see what will happen
-   */
+  const pathName = useRouter().pathname
+
+  const getPathProps = pathProps.bind(null, pathName, colorMode)
+
   const { scrollY } = useViewportScroll()
+
   React.useEffect(() => {
     return scrollY.onChange(() => setY(scrollY.get()))
   }, [scrollY])
 
+  const scrolledToTop = y >= 64
+
   return (
-    <chakra.header
-      ref={ref}
-      pos={y >= 74 ? 'fixed' : 'static'}
-      transform={y >= 74 ? 'translateZ(100px)' : undefined}
-      boxShadow={y >= 74 ? `0 0 0 1px ${shadow}` : undefined}
-      top="0"
-      zIndex="3000"
-      bg={bg}
-      left="0"
-      right="0"
-      width="full"
-      {...props}
-    >
-      {props.children}
+    <chakra.header h={height}>
+      <Box
+        position={scrolledToTop ? 'fixed' : 'static'}
+        transform={scrolledToTop ? 'translateZ(100px)' : undefined}
+        boxShadow={scrolledToTop ? `0 0 0 1px ${shadow}` : undefined}
+        ref={ref}
+        transition="box-shadow .2s ease"
+        top="0"
+        zIndex="3000"
+        bg={bg}
+        left="0"
+        right="0"
+        width="full"
+        h={height}
+      >
+        <Flex
+          flexDir="row"
+          alignItems="center"
+          justifyContent="flex-start"
+          maxW="container.lg"
+          px="10"
+          mx="auto"
+        >
+          <Stack direction="row" spacing="8">
+            <Link href="/">
+              <a>
+                <Text
+                  borderBottomWidth="2px"
+                  py="3"
+                  borderBottomColor="white"
+                  {...getPathProps('/')}
+                >
+                  Overview
+                </Text>
+              </a>
+            </Link>
+            <Link href="/projects">
+              <a>
+                <Text
+                  borderBottomWidth="2px"
+                  py="3"
+                  borderBottomColor="white"
+                  {...getPathProps('/projects')}
+                >
+                  projects
+                </Text>
+              </a>
+            </Link>
+            <Link href="/activity">
+              <a>
+                <Text
+                  borderBottomWidth="2px"
+                  py="3"
+                  borderBottomColor="white"
+                  {...getPathProps('/activity')}
+                >
+                  Activity
+                </Text>
+              </a>
+            </Link>
+            <Link href="/settings">
+              <a>
+                <Text
+                  borderBottomWidth="2px"
+                  py="3"
+                  borderBottomColor="white"
+                  {...getPathProps('/settings')}
+                >
+                  Settings
+                </Text>
+              </a>
+            </Link>
+          </Stack>
+        </Flex>
+      </Box>
     </chakra.header>
   )
 }
-;``
