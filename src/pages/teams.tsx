678042135ca427cb13ec7a0ed0e211d63f681783
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-23 21:42:07 +0530

    added teams page

---
+++ src/pages/teams.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Page } from '../components/Page'
+
+const Teams = () => (
+  <Page>
+    <Container height="100vh">
+      <Hero />
+    </Container>
+  </Page>
+)
+
+export default Teams
