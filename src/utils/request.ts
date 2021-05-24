7bd679aa1fb2d1b5d29133fedd8697cc427fbd22
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-24 21:35:19 +0530

    updated the query fetcher to have dynamic headers

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,12 @@
 import { GraphQLClient } from 'graphql-request'
 
-export function queryFetcher(query: string, variables: Object, token: string) {
+export function queryFetcher(query: string, variables: Object, token: string, headers?: {}) {
   return new GraphQLClient(String(process.env.NEXT_PUBLIC_GRAPHQL_URL), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
+    headers: headers
+      ? headers
+      : {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
   }).request(query, variables)
 }
