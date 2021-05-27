00347458862a9c90fcbba47fb011c1d855f8cfa1
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-27 22:32:35 +0530

    somewhat fixed the favicon issue

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import type { FC } from 'react'
 import { NextSeo } from 'next-seo'
 import Head from 'next/head'
 
@@ -8,19 +7,21 @@ import { Footer } from './Footer'
 import { Header } from './Header'
 import { useColorModeValue } from '@chakra-ui/react'
 
+import type { FC } from 'react'
+
 type PageProps = {
   title: string
   description: string
 }
 
 export const Page: FC<PageProps> = (props) => {
-  const iconPath = useColorModeValue('/images/dark.png', '/images/light.png')
+  const iconPath = useColorModeValue('../public/images/dark.png', '../public/images/light.png')
   return (
     <>
       <NextSeo title={props?.title} description={props.description} />
       <Head>
         {/* <link rel="icon" href={iconPath} /> */}
-        <link rel="shortcut icon" href={iconPath} />
+        <link rel="icon" href={iconPath} />
       </Head>
       <Header />
       <Main>{props.children}</Main>
