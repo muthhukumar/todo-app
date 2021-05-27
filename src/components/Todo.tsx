8805ae106c1f09c2ceef9813aaebe18c0a3a2227
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-05-27 22:33:43 +0530

    fixed the header in the todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { FC } from 'react'
-import { Text, Flex, IconButton, useColorModeValue } from '@chakra-ui/react'
+import { Text, Flex, IconButton, useColorModeValue, ButtonGroup } from '@chakra-ui/react'
 import { FaCheck } from 'react-icons/fa'
 import { ImRadioUnchecked } from 'react-icons/im'
 import { GoX } from 'react-icons/go'
@@ -51,37 +51,39 @@ export const Todo: FC<PropsType> = (props) => {
         p="1"
         borderBottomWidth="1px"
       >
-        {/* <Text fontSize="sm" ml="3">{moment(createdAt).calendar()}</Text> */}
-        <Text fontSize="sm" ml="3">
-          {moment(createdAt).format('ll')}
-        </Text>
+        <Flex alignItems="center">
+          <Text fontSize="sm" ml="3">
+            {moment(createdAt).format('ll')}
+          </Text>
+        </Flex>
         <Flex
           alignItems="center"
           p="1px"
           borderWidth="1px"
           borderBottomLeftRadius="5px"
           borderTopRightRadius="5px"
-          borderColor={done ? '#50e3c1f5' : '#ff0102d4'}
         >
-          <IconButton
-            aria-label={done ? 'Mark as pending' : 'Mark as complete'}
-            icon={done ? <ImRadioUnchecked /> : <FaCheck />}
-            onClick={onToggleDone}
-            color={done ? '#50e3c1f5' : '#ff0102d4'}
-            rounded="none"
-            size="xs"
-            bg={bg}
-          />
-          <IconButton
-            rounded="none"
-            color={done ? '#50e3c1f5' : '#ff0102d4'}
-            aria-label="Delete item"
-            icon={<GoX />}
-            fontSize="md"
-            onClick={handleDelete}
-            size="xs"
-            bg={bg}
-          />
+          <ButtonGroup isAttached>
+            <IconButton
+              aria-label={done ? 'Mark as pending' : 'Mark as complete'}
+              icon={done ? <ImRadioUnchecked /> : <FaCheck />}
+              onClick={onToggleDone}
+              color={done ? '#50e3c1f5' : '#ff0102d4'}
+              rounded="none"
+              size="xs"
+              bg={bg}
+            />
+            <IconButton
+              rounded="none"
+              color={done ? '#50e3c1f5' : '#ff0102d4'}
+              aria-label="Delete item"
+              icon={<GoX />}
+              fontSize="md"
+              onClick={handleDelete}
+              size="xs"
+              bg={bg}
+            />
+          </ButtonGroup>
         </Flex>
       </Flex>
       <Text m="4" mt="3" fontSize="md">
